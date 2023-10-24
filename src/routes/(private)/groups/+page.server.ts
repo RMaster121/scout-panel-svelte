a6d331862acf5f328764c7eafb4e19aa41c47efb
RMaster121 <rs.szczerba@hotmail.com>
2023-10-24 20:52:33 +0200

    Changed groups order

--- src/routes/(private)/groups/+page.server.ts
+++ src/routes/(private)/groups/+page.server.ts
@@ -1,6 +1,6 @@
 export async function load({ locals: { supabase } }) {
     //supabase fetch groups
-    const { data: groups, error } = await supabase.from('small_groups').select('*');
+    const { data: groups, error } = await supabase.from('small_groups').select('*').order('name');
     if (error) throw error;
     return { groups };
 };
