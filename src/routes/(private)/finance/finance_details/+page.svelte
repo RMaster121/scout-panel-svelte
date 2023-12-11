703d44b56fbf7f4119eacfdaa04524951782289c
RMaster121 <rs.szczerba@hotmail.com>
2023-12-11 18:54:36 +0100

    Added finance_details dialog\

--- src/routes/(private)/finance/finance_details/+page.svelte
+++ src/routes/(private)/finance/finance_details/+page.svelte
@@ -6,6 +6,7 @@
 	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
 	import DetailsPage from '$lib/components/DetailsPage.svelte';
+	import Dialog from '$lib/components/Dialog.svelte';
 	export let data: PageData;
 
 	let returnPath = '/finance';
@@ -17,7 +18,6 @@
 			returnPath = urlParams.get('return');
 		}
 		selectedCategory = data.finance.fk_finance_category_id;
-		console.log(data); //TODO: remove
 	});
 
 	const saveOperation = async (event) => {
@@ -75,21 +75,7 @@
 		}
 	};
 
-	const removeCategory = async (id) => {
-		const res = await fetch(`/api/finance/category/` + id, {
-			method: 'DELETE'
-		}).then((res) => res.json());
-		const { status, body } = res;
-		if (status === 204) {
-			toast.success('Usunięto!');
-			goto(returnPath, { invalidateAll: true });
-		} else {
-			toast.error('Wystąpił błąd! - ' + res);
-			console.log('Error:', status, body);
-		}
-	};
-
-	const openDialog = (category:number|null = null) => {
+	const openDialog = (category: number | null = null) => {
 		selectedCategory = category;
 		(document.getElementById('category_modal') as HTMLFormElement).showModal();
 	};
@@ -154,7 +140,23 @@
 			<option value="" selected={selectedCategory === null}>Brak</option>
 			{#each data.categories as category}
 				<option value={category.id} selected={selectedCategory === category.id}>
-					{category.name}
+					<span class="flex items-center justify-between">
+						<span>{category.name}</span>
+						<svg
+							class="w-4 h-4"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+							/>
+						</svg>
+					</span>
 				</option>
 			{/each}
 			<option on:click|preventDefault={() => openDialog()} value="">Dodaj nową kategorię</option>
@@ -241,99 +243,26 @@
 	</div>
 </dialog>
 
-<dialog id="category_modal" class="modal">
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<div class="modal-backdrop" on:click|stopPropagation={closeCategoryDialog} />
-	<div class="border rounded-lg shadow relative max-w-2xl modal-box">
-		<div class="flex justify-end p-2">
-			<button
-				type="button"
-				class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-				on:click={closeCategoryDialog}
-			>
-				<svg
-					class="w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/></svg
-				>
-			</button>
-		</div>
-
-		<div class="p-6 pt-0 text-center">
-			<svg
-				class="w-60 h-20 text-red-600 mx-auto"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-				/>
-			</svg>
-			<h2 class="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-				Uprawnienia dostępu
-			</h2>
-			{#if !selectedCategory}
-				<p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">
-					Dodaj nową kategorię transakcji
-				</p>
-				<form id="email-form" method="dialog" on:submit={addCategory}>
-					<label for="email">Nazwa kategorii</label>
-					<input
-						type="text"
-						name="category_name"
-						id="category_name"
-						placeholder="Materiały"
-						required
-					/>
-				</form>
-				<div class="flex justify-center mt-5">
-					<button
-						form="email-form"
-						class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
-					>
-						Tak, dodaj
-					</button>
-					<form method="dialog" on:submit={closeCategoryDialog}>
-						<button
-							class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
-						>
-							Nie, anuluj
-						</button>
-					</form>
-				</div>
-			{/if}
-			{#if selectedCategory}
-				<p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">
-					Czy na pewno chcesz usunąć kategorię <b>{selectedCategory}</b>?
-				</p>
-				<div class="flex justify-center mt-5">
-					<button
-						class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
-					on:click={() => {removeCategory(selectedCategory)}}
-						>
-						Tak, usuń
-					</button>
-					<form method="dialog" on:submit={closeCategoryDialog}>
-						<button
-							class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
-						>
-							Anuluj
-						</button>
-					</form>
-				</div>
-			{/if}
-		</div>
+<Dialog>
+	<p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">
+		Dodaj nową kategorię transakcji
+	</p>
+	<form id="email-form" method="dialog" on:submit={addCategory}>
+		<label for="email">Nazwa kategorii</label>
+		<input type="text" name="category_name" id="category_name" placeholder="Materiały" required />
+	</form>
+	<div class="flex justify-center mt-5">
+		<button
+			form="email-form"
+			class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
+		>
+			Tak, dodaj
+		</button>
+		<button
+			class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
+			on:click|preventDefault={closeCategoryDialog}
+		>
+			Nie, anuluj
+		</button>
 	</div>
-</dialog>
+</Dialog>
