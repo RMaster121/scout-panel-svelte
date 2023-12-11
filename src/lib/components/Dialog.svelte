5d42ab87b76f93bd96f8769fd96ce4eed02505bf
RMaster121 <rs.szczerba@hotmail.com>
2023-12-11 18:52:00 +0100

    Add Dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    export let id;
+
+    /** It is possible to override default function with custom one. In that case you have to handle closing dialog yourself. */
+    export let closeDialogOverride : void | null = null;
+
+    export let title : string;
+
+    function closeDialog() {
+        if (!typeof(closeDialogOverride) != null) {
+            closeDialogOverride;
+        } else {
+            (document.getElementById(id) as HTMLFormElement).close();
+        }
+	}
+</script>
+
+<dialog id={id} class="modal">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div class="modal-backdrop" on:click|stopPropagation={closeDialog} />
+	<div class="border rounded-lg shadow relative max-w-2xl modal-box">
+		<div class="flex justify-end p-2">
+			<button
+				type="button"
+				class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+				on:click={closeDialog}
+			>
+				<svg
+					class="w-5 h-5"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+		</div>
+
+		<div class="p-6 pt-0 text-center">
+			<svg
+				class="w-60 h-20 text-red-600 mx-auto"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				/>
+			</svg>
+			<h2 class="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+				{title}
+			</h2>
+			<slot />
+		</div>
+	</div>
+</dialog>
